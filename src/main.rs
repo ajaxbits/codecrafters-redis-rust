2560792deecfe79c03ae599a3273e540ec4cb4ab
Alex Jackson <contact@ajaxbits.com>
2022-08-09 21:58:36 +0000

    respond PONG to all requests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
-use std::fs;
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50];
+    match stream.read(&mut data) {
+        Ok(_) => {
+            stream.write(b"+PONG").unwrap();
+        }
+        Err(_) => {
+            println!(
+                "An error occured! Terminating connection with {}",
+                stream.peer_addr().unwrap()
+            );
+            stream
+                .shutdown(std::net::Shutdown::Both)
+                .expect("could not shutdown connection");
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,8 +26,10 @@ fn main() {
     // Uncomment this block to pass the first stage
     // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     let listener = TcpListener::bind("127.0.0.1:6379").expect("could not bind to port");
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-        Err(e) => println!("couldn't accept client: {:?}", e),
+    for stream_result in listener.incoming() {
+        match stream_result {
+            Ok(stream) => handle_connection(stream),
+            Err(e) => println!("couldn't accept client: {:?}", e),
+        }
     }
 }
