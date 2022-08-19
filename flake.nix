4f1f265b0b386a7cda7fb6877b9e1359dcb71dcf
Alex Jackson <contact@ajaxbits.com>
2022-08-19 15:27:46 +0000

    switch to rustup

--- flake.nix
+++ flake.nix
@@ -1,54 +1,19 @@
 {
-  description = "codecrafters rust track redis";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          srcNoTarget = dir:
-            builtins.filterSource
-              (path: type: type != "directory" || builtins.baseNameOf path != "target")
-              dir;
-
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-          rustEnv = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
-          naersk-lib = naersk.lib."${system}";
-
-          src = srcNoTarget ./.;
-          blog = naersk-lib.buildPackage {
-            inherit src;
-            name = "xeBlogChallenge2022";
-            buildInputs = [
-              pkgs.openssl
-            ];
-            remapPathPrefix = true;
-          };
-
-        in
-        rec {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                rustEnv
-                openssl
-                pkgconfig
-                cargo-edit
-                cargo-watch
-              ];
-          };
-        });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [ 
+          rustup
+          openssl
+          pkgconfig
+        ];
+      };
+    });
 }
